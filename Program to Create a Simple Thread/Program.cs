e6a59ca1251e4c3a8a98b09b4549bc4aef222289
roshangurung <r.gurung1992@gmail.com>
2019-12-10 19:56:05 -0800

    Initial commit

---
+++ Program to Create a Simple Thread/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+class program///This C# Program, we are creating a new ThreadStart delegate. The delegate points to a method that will be executed by the new thread. Pass this delegate as a parameter when creating a new Thread instance. Finally, call the Thread.Start method to run the method.//
+{
+    public void WorkThreadFunction()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Simple Thread");
+        }
+    }
+}
+class threprog
+{
+    public static void Main()
+    {
+        program pg = new program();
+        Thread thread = new Thread(new ThreadStart(pg.WorkThreadFunction));
+        thread.Start();
+        Console.Read();
+    }
+}
+///Output:Simple Thread
+//Simple Thread
+//Simple Thread
+//Simple Thread
+//Simple Thread
